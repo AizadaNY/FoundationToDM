beb4125c8b6e74416c2518aebf232c26f76da04f
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-01-29 20:25:34 -0500

    week15

---
+++ src/main/java/week15/LinkedListCycle2.java
@@ -0,0 +1,41 @@
+package week15;
+
+public class LinkedListCycle2 {
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode slow=head;
+            ListNode fast=head;
+            int counter=1;
+            while(fast!= null && fast.next !=null){
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast){
+                    break;
+                }
+            }
+            if(fast==null || fast.next == null){
+                return null;
+            }
+
+            slow=head;
+            while(slow!=fast){
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+
+        }
+    }
+}
