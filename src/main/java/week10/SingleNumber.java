1dd603ded38a36982a6d9da4ff6d2859acf1e725
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-02-02 16:45:57 -0500

    week

--- src/main/java/week10/SingleNumber.java
+++ src/main/java/week10/SingleNumber.java
@@ -1,4 +1,11 @@
 package week10;
 
 public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int answer = 0;
+        for(int x : nums){
+            answer = answer ^ x;
+        }
+        return answer;
+    }
 }
--- src/main/java/week10/SnakeInMatrix.java
+++ src/main/java/week10/SnakeInMatrix.java
@@ -1,4 +1,21 @@
 package week10;
 
+import java.util.List;
+
 public class SnakeInMatrix {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int number = 0;
+        for(String command : commands){
+            if(command.equals("UP")){
+                number -= n;
+            }else if(command.equals("DOWN")){
+                number +=n;
+            }else if(command.equals("RIGHT")){
+                number++;
+            }else if(command.equals("LEFT")){
+                number--;
+            }
+        }
+        return number;
+    }
 }
