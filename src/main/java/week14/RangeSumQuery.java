e6e28afb4c2f470a63b8f6121e8cf3431eae3056
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-02-01 22:39:55 -0500

    week15

---
+++ src/main/java/week14/RangeSumQuery.java
@@ -0,0 +1,28 @@
+package week14;
+
+public class RangeSumQuery {
+    int[] prefix;
+
+    public void NumArray(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n+1];
+        prefix[0] = 0;
+        for( int i = 1; i <= n; i++){
+            prefix[i] = prefix[i-1]+ nums[i-1];
+        }
+    }
+
+
+    //will use prefix
+    public int sumRange(int left, int right) {
+        left++; right++;
+        return prefix[right] - prefix[left - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+
--- src/main/java/week14/SortArrayByParity2.java
+++ src/main/java/week14/SortArrayByParity2.java
@@ -1,4 +1,33 @@
 package week14;
 
 public class SortArrayByParity2 {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i = 0;
+        int j = 1;
+        int n = nums.length;
+
+        while(i < n && j < n){
+
+            while(i < n && nums[i] % 2 == 0){
+                i += 2;
+            }
+
+            while(j < n && nums[j] % 2 ==1){
+                j +=2;
+            }
+
+            if(i < n && j < n){
+                swap(nums, i , j);
+            }
+        }
+
+        return nums;
+
+    }
+
+    public void swap(int[] nums , int start, int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
 }
