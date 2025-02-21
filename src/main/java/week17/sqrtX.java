e1f168fd3910cd33b14eda1fd98034eab8b6cbf5
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-02-20 22:43:05 -0500

    week17

--- src/main/java/week17/sqrtX.java
+++ src/main/java/week17/sqrtX.java
@@ -1,4 +1,25 @@
 package week17;
 
 public class sqrtX {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        int answer = -1;
+        int left = 1;
+        int right = x;
+
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if ((long) middle * middle > (long) x) {
+
+                right = middle - 1;
+            } else {
+                answer = middle;
+                left = middle + 1;
+            }
+        }
+        return answer;
+    }
+
+
 }
