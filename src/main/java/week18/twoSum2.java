ea28cced2cc39f5b3ec46bf4ed0d6c7a7cb00925
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-02-24 23:34:53 -0500

    week18

---
+++ src/main/java/week18/twoSum2.java
@@ -0,0 +1,15 @@
+package week18;
+
+public class twoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(numbers[left] + numbers[right] != target){
+            if(numbers[left] + numbers[right] < target) left++;
+            else right --;
+        }
+
+        return new int[] {left + 1, right + 1} ;
+    }
+}
