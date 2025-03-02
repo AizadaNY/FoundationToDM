517ab70452a553a48526f7b8bd66b4fe911cd6dc
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-03-02 13:51:31 -0500

    week18

---
+++ src/main/java/week18/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package week18;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length -1;
+        int maxArea = 0;
+
+        while(left < right){
+            int w = right- left;
+            int h = Math.min(height[left], height[right]);
+            int v = w * h;
+
+            maxArea = Math.max(maxArea , v);
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
