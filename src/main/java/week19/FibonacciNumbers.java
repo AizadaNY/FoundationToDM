ba3fa78136111942ec623cb6bf4f551bb193b1ed
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-03-02 20:48:04 -0500

    fibbonaci number

--- src/main/java/week19/FibonacciNumbers.java
+++ src/main/java/week19/FibonacciNumbers.java
@@ -3,8 +3,8 @@ package week19;
 public class FibonacciNumbers {
 
     public int fib(int n) {
-        int sum = 0;
-        if(n <=0){
+
+        if (n <= 1) {
             return n;
         }
         return fib(n - 1) + fib(n - 2);
