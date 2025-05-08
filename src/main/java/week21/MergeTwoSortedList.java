59e2b49ac2146260bfbc4f7248218c49637dcd9c
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-05-08 19:53:33 -0400

    week21

--- src/main/java/week21/MergeTwoSortedList.java
+++ src/main/java/week21/MergeTwoSortedList.java
@@ -18,3 +18,4 @@ public class MergeTwoSortedList {
         return list2;
     }
 }
+e
--- src/main/java/week21/ReverseLinkedList.java
+++ src/main/java/week21/ReverseLinkedList.java
@@ -1,4 +1,26 @@
 package week21;
 
+import week15.ListNode;
+
 public class ReverseLinkedList {
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+
+            return reverseListNode(head, null);
+        }
+
+        public ListNode reverseListNode(ListNode head, ListNode newHead) {
+            if(head == null){
+                return newHead;
+            }
+
+            ListNode next = head.next;
+            head.next = newHead;
+
+            return reverseListNode(next, head);
+
+        }
+    }
+
 }
