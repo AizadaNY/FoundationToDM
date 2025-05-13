b2635a9389029b17fb2b814350b3d50962f1a872
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2025-05-12 20:38:06 -0400

    week21

---
+++ src/main/java/week22/ValisPharanteses.java
@@ -0,0 +1,32 @@
+package week22;
+
+import java.util.Stack;
+
+public class ValisPharanteses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char top = stack.peek();
+
+                if((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
